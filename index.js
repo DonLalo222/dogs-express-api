b0f8176370e8442528cfbd58a90853378e83f5fc
Eduardo Gomez <g.vasquez.eduardo@gmail.com>
2021-11-30 13:24:23 -0300

    set port

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import helmet from 'helmet';
 import { router as dogRouter } from "./routes/dogs.js";
 
 const app = express();
-const port = 3000;
+
+app.set('port', (process.env.PORT || 5000));
 
 app.use(helmet());
 // routes
 app.use('/api/dogs', dogRouter);
 
-app.listen(port, () => {
-  console.log(`Api listening at http://localhost:${port}`)
+app.listen(app.get('port'), () => {
+  console.log(`Api listening at http://localhost:${app.get('port')}`)
 })
